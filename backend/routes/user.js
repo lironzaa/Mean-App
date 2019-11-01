cdfe2761a7de273fd97f167abc31bda178e818f0
lironzaa <lironzaa@gmail>
2019-11-02 01:15:53 +0200

    Using Controllers

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,65 +1,9 @@
 const express = require('express');
-const User = require('../models/user');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const UserController = require('../controllers/user');
 
-router.post('/signup', (req, res) => {
-  bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(result => {
-          res.status(201).json({
-            message: 'User created!',
-            result: result
-          })
-        })
-        .catch(err => {
-          res.status(500).json({
-            message: "Invalid auth credentials"
-          })
-        })
-    })
-})
+router.post('/signup', UserController.createUser);
 
-router.post('/login', (req, res) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(404).json({
-          message: 'Auth failed'
-        })
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password)
-    })
-    .then(result => {
-      if (!result) {
-        return res.status(404).json({
-          message: 'Auth failed'
-        })
-      }
-      const token = jwt.sign(
-        { email: fetchedUser.email, userId: fetchedUser._id },
-        'secret_JWT',
-        { expiresIn: '1h' }
-      );
-      res.status(200).json({
-        token,
-        expiresIn: 3600,
-        userId: fetchedUser._id
-      })
-    })
-    .catch(err => {
-      return res.status(404).json({
-        message: 'Invalid auth credentials'
-      })
-    })
-});
+router.post('/login', UserController.userLogin);
 
 module.exports = router;
